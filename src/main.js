5e8c5a8c4b1ba6c3522d1f8557f187bcc0fcb456
Alvaro Calle <alvcm13@gmail.com>
2022-11-03 11:28:46 -0400

    solve addition, subtraction, multiplication, and division

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ const screenText = document.getElementById("screen-text");
 const screenInput = document.getElementById("screen-input");
 
 const buttons = document.getElementById("buttons");
+const equal = document.getElementById("equal");
 
 let lastDigit = "0";
+let operation = [];
 
 buttons.addEventListener("click", (event) => {
     if (event.target.nodeName === "INPUT") {
@@ -32,3 +34,75 @@ buttons.addEventListener("click", (event) => {
         lastDigit = event.target.value;
     }
 });
+
+equal.onclick = generateOperation;
+
+function generateOperation() {
+    if (screenText.value !== "") {
+        screenText.value = screenText.value + " " + screenInput.value;  
+        operation = screenText.value.trim().split(" ");
+        screenText.value = screenText.value + " =";  
+
+        const signs = ["*","/", "+", "-"];
+        signs.forEach(sign => solveOperation(sign))
+        screenInput.value = operation[0];
+    }
+}
+
+function solveOperation(sign) {
+    const index = operation.indexOf(sign);
+
+    if (index === -1) {
+        return;
+    } else {
+        let res =  0;
+        const num1 = Number(operation[index - 1]);
+        const num2 = Number(operation[index + 1]);
+        switch (sign) {
+            case "+":
+                res = add(num1, num2);
+                operation[index-1] = res;
+                operation.splice(index,2)
+                break;
+            case "-":
+                res = sub(num1, num2);
+                operation[index-1] = res;
+                operation.splice(index,2)
+                break;
+
+            case "*":
+                res = multiply(num1, num2);
+                operation[index-1] = res;
+                operation.splice(index,2)
+                break;
+
+            case "/":
+                res = divide(num1, num2);
+                operation[index-1] = res;
+                operation.splice(index,2)
+                break;
+        
+            default:
+                break;
+        }
+        solveOperation(sign)
+    }
+}
+
+//operations
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
